1d484cadec738d717dcd337a6c347f2e53269610
Peter Nied <petern@amazon.com>
2012-11-08 16:20:35 -0800

    Basic frequency report

--- src/peter/nied/ExampleCompareBooks.java
+++ src/peter/nied/ExampleCompareBooks.java
@@ -9,7 +9,7 @@ public class ExampleCompareBooks
      * Compare the word counts of the two books:
      * A Jounery to the Interior of the Earth by Julie Vern
      * &
-     * The Raven by Edgar Allan Poe"
+     * The Raven by Edgar Allan Poe
      */
     public static void main(String[] args)
     {
@@ -19,13 +19,6 @@ public class ExampleCompareBooks
         final List<String> vernRawWords = vern.asWords(vernParser);
         final List<WordFrequency> vernWordFrequencies = WordFrequencyUtilties.getWordFrequencies(vernRawWords);
 
-        System.out.println("Vern words: " + vernWordFrequencies.size());
-        for (int i = 0; i < 10; i++)
-        {
-            final WordFrequency frequencyValue = vernWordFrequencies.get(i);
-            System.out.println(frequencyValue.mWord + " " + frequencyValue.mFrequency);
-        }
-
         final IBook poe = LocalBook.getNewBookFromPath(".\\test-content\\the_raven.htm");
         final MutlilineWordParser parser = new MutlilineWordParser();
         parser.setOnlyStartCountingAfterBodyTag(true);
@@ -33,12 +26,6 @@ public class ExampleCompareBooks
 
         final List<WordFrequency> poeWordFrequencies = WordFrequencyUtilties.getWordFrequencies(poeRawWords);
 
-        System.out.println("Poe words: " + poeRawWords.size());
-        for (int i = 0; i < 10; i++)
-        {
-            final WordFrequency frequencyValue = poeWordFrequencies.get(i);
-            System.out.println(frequencyValue.mWord + " " + frequencyValue.mFrequency);
-        }
+        WordFrequencyUtilties.printFrequencyReport("Vern", vernWordFrequencies, "Poe", poeWordFrequencies);
     }
-
 }
--- src/peter/nied/WordFrequencyUtilties.java
+++ src/peter/nied/WordFrequencyUtilties.java
@@ -1,15 +1,22 @@
 
 package peter.nied;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Map.Entry;
 
 public class WordFrequencyUtilties
 {
+    // Just use System.out for the time being, we can fix this later to output to a file or something fancy
+    protected static final PrintStream mWriter = System.out;
+    protected static Queue<String> mLineBuffer = new LinkedList<String>();
+    
     public static List<WordFrequency> getWordFrequencies(final List<String> poeRawWords)
     {
         // Take everything from the raw words an stick it in a map and start calculating values
@@ -64,4 +71,71 @@ public class WordFrequencyUtilties
         }
     }
 
+    public static void printFrequencyReport(final String author1, final List<WordFrequency> frequencies1,
+            final String author2, final List<WordFrequency> frequencies2)
+    {
+        // Handle the first rows, we are currently configured for 6 colums, with a empty column between datasets
+        mLineBuffer.add(author1);
+        mLineBuffer.add("");
+        mLineBuffer.add("");
+        mLineBuffer.add(author2);
+        mLineBuffer.add("");
+        mLineBuffer.add("");
+        writeData();
+
+        // Handle the content titles
+        mLineBuffer.add("Word");
+        mLineBuffer.add("Hits");
+        mLineBuffer.add("");
+        mLineBuffer.add("Word");
+        mLineBuffer.add("Hits");
+        mLineBuffer.add("");
+        writeData();
+
+        // Deal with pushing out real data
+        for (int i = 0; i < 10; i++)
+        {
+            if (frequencies1.size() > i)
+            {
+                final WordFrequency wf1 = frequencies1.get(i);
+                mLineBuffer.add(wf1.mWord);
+                mLineBuffer.add(wf1.mFrequency + "");
+                mLineBuffer.add("");
+            }
+            else
+            {
+                mLineBuffer.add("");
+                mLineBuffer.add("");
+                mLineBuffer.add("");
+            }
+
+            if (frequencies2.size() > i)
+            {
+                final WordFrequency wf2 = frequencies2.get(i);
+                mLineBuffer.add(wf2.mWord);
+                mLineBuffer.add(wf2.mFrequency + "");
+                mLineBuffer.add("");
+            }
+            else
+            {
+                mLineBuffer.add("");
+                mLineBuffer.add("");
+                mLineBuffer.add("");
+            }
+            writeData();
+        }
+    }
+
+    private static void writeData()
+    {
+        final StringBuilder sb = new StringBuilder();
+        for (final String s : mLineBuffer)
+        {
+            sb.append(s);
+            sb.append('\t');
+        }
+
+        mWriter.println(sb.toString());
+        mLineBuffer.clear();
+    }
 }
